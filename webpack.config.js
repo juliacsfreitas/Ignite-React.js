e04f4ff7d4ca55a60eecefab3165ee88a20c57f0
juliacsfreitas <freitas.jucsf@gmail.com>
2022-06-22 22:59:16 +0100

    configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path") 
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [ ".js", ".jsx"],
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            }
+        ],
+    }
+
+ };
+
