5974a91bdf46f764f91868f11ed5e607e02efa09
juliacsfreitas <freitas.jucsf@gmail.com>
2022-07-27 20:49:34 +0100

    repository correction

--- src/Components/Repositorylist.tsx
+++ src/Components/Repositorylist.tsx
@@ -3,37 +3,27 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../Styles/repositories.scss';
 
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(Response => Response.json())
+      .then(data => setRepositories(data))
+  }, []);
 
 
-const repository = {
-    name:'unform',
-    description: 'Forms in React',
-    link: 'http://github.com/unform/unfor'
-}
+  return (
+    <section className="repository-list">
+      <h1> Lista de repositórios</h1>
 
-
-export function RepositoryList () {
-const [repositories, setRepositories] = useState([]);
-
-useEffect(() => {
-fetch('https://api.github.com/orgs/rocketseat/repos')
-.then(Response => Response.json())
-.then(data => setRepositories(data))
-}, []);
-
-
-    return (
-        <section className="repository-list">
-            <h1> Lista de repositórios</h1>
-
-            <ul>
-                <RepositoryItem repository= {repository}/> 
-                <RepositoryItem repository= {repository}/> 
-                <RepositoryItem repository= {repository}/>
-                <RepositoryItem repository= {repository}/>
-          </ul>
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem repository={repository} />
+        }
+        )}
+        </ul>
         </section>
-        
 
-    )
-}
+        );
+}  
