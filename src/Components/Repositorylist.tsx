e4ec8d46cc0afe0e6c6459b4ff72e0d3c833fc72
juliacsfreitas <freitas.jucsf@gmail.com>
2022-08-01 05:18:43 +0100

    repository correction

--- src/Components/Repositorylist.tsx
+++ src/Components/Repositorylist.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
 
 import '../Styles/repositories.scss';
 
